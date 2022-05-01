bb42dbbf1d06f394394d02a0fcf57d2066dac58d
gunwoof <gunwoof1234@naver.com>
2022-05-01 15:17:19 +0900

    선택정렬 문제

--- 정렬/2750.py
+++ 정렬/2750.py
@@ -1,4 +1,4 @@
-from sys import stdin as s
+f'''rom sys import stdin as s
 
 n = int(s.readline())
 number=[int(s.readline()) for i in range(n)]
@@ -9,6 +9,23 @@ for i in range(n):
     for j in range(i+1,n):
         if min > number[j]:
             min = number[j]
+            min_index = j
+    number[i],number[min_index] = number[min_index],number[i]
+
+for i in number:
+    print(i)'''
+
+
+# 더 깔끔한 코드
+from sys import stdin as s
+
+n = int(s.readline())
+number=[int(s.readline()) for i in range(n)]
+
+for i in range(n):
+    min_index = i
+    for j in range(i+1,n):
+        if number[min_index] > number[j]:
             min_index = j
     number[i],number[min_index] = number[min_index],number[i]
 
