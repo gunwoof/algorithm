340cf53af51930073f0028a2750378dda8d6887a
gunwoof <gunwoof1234@naver.com>
2022-05-11 23:56:15 +0900

    괄호 문제

--- 스택/9012.py
+++ 스택/9012.py
@@ -41,6 +41,25 @@ for i in range(T):
 
     
 
+# 누군가의 깔끔한 코드
 
-
+for i in range(int(s.readline())):
+    VPSstring = s.readline()
+    stk = []
+    check = False
+    for k in VPSstring:
+        if k == "(":
+            stk.append(k)
+        elif k == ")":
+            if stk:
+                stk.pop()
+            else:
+                check = True
     
+    # 핵심 : NO가 나오는 두가지경우를 한번에 체크
+    # 1.여는괄호가 없는 상태에서 닫는괄호가 나올때
+    # 2. 여는괄호와 닫는괄호의 개수가 다를 때
+    if stk or check: 
+        print("NO")
+    else:
+        print("YES")
