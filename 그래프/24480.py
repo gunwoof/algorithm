5db031d7bf8d019761e827205e0788c714f6586b
gunwoof <gunwoof1234@naver.com>
2022-06-13 19:44:19 +0900

    깊이 우선 탐색 2 문제

---
+++ 그래프/24480.py
@@ -0,0 +1,34 @@
+from sys import stdin as s
+
+N,M,R=map(int,s.readline().split())
+graph=[[] for _ in range(N+1)]
+
+
+stack=[]
+visited=[0 for _ in range(N+1)]
+visited_cnt=1
+
+for i in range(M):
+    x,y=map(int,s.readline().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+for i in range(N+1):
+    graph[i].sort() 
+
+stack.append(R)
+'''visited[R]=visited_cnt
+visited_cnt +=1'''
+
+while stack:
+    compare_node=stack.pop()
+    if visited[compare_node] ==0:
+        visited[compare_node]=visited_cnt
+        visited_cnt +=1
+
+    for node in graph[compare_node]:
+        if visited[node] == 0:
+            stack.append(node)
+ 
+for i in visited[1::]:
+    print(i)
